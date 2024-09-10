345cce9b0a48f9202be22c587785c01327fa5b2e
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-11 01:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+     constructor(width,height){
+          this._width = width
+          this._height = height
+     }
 
-class Square extends Animal {}
+     get width(){
+          return `${this._width}`
+     }
+
+     get height(){
+          return `${this._height}`
+     }
+
+     getArea() {
+          const area = `${this._width}` * `${this.height}`
+     }
+}
+
+class Square extends Animal {
+     constructor(side){
+          this._side = side
+     }
+
+     getPerimeter(){
+          const perimeter = 4 * `${this._side}`
+     }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
